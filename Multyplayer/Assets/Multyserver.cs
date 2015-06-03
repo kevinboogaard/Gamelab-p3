ec4bab961897e5a83e926a60f2f7e6de70566943
kevinboogaard <kevin.boogaard3@gmail.com>
2015-06-03 20:02:37 +0200

    second
    multiplayer

---
+++ Multyplayer/Assets/Multyserver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Multyserver : MonoBehaviour {
+
+    public Transform cubePrefab;
+
+    void OnServerInitialized()
+    {
+        SpawnPlayer();
+    }
+
+    void OnConnectedToServer()
+    {
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        Transform myTransform = (Transform)Network.Instantiate(cubePrefab, transform.position, transform.rotation, 0);
+    }
+}
---
+++ Multyplayer/Assets/ServerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerMovement : MonoBehaviour
+{
+
+    void Awake()
+    {
+        if (!GetComponent<NetworkView>().isMine)
+            enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (GetComponent<NetworkView>().isMine)
+        {
+            Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            float speed = 5;
+            transform.Translate(speed * moveDir * Time.deltaTime);
+
+        }
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            Vector3 myPosition = transform.position;
+            stream.Serialize(ref myPosition);
+        }
+        else
+        {
+            Vector3 receivedPosition = Vector3.zero;
+            stream.Serialize(ref receivedPosition);
+            transform.position = receivedPosition;
+        }
+    }
+}
