f9609af024884dca0c4d144f136b8d0ec974598f
kevinboogaard <kevin.boogaard3@gmail.com>
2015-06-08 12:16:46 +0200

    fix2

--- Multyplayer/Assets/_scripts/Snake/Snake.cs
+++ Multyplayer/Assets/_scripts/Snake/Snake.cs
@@ -18,6 +18,27 @@ public class Snake : MonoBehaviour {
         InvokeRepeating("Move", speed, speed);
     }
 
+    void Awake()
+    {
+        if (!GetComponent<NetworkView>().isMine)
+            enabled = false;
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            Vector3 myPosition = transform.position;
+            stream.Serialize(ref myPosition);
+        }
+        else
+        {
+            Vector3 receivedPosition = Vector3.zero;
+            stream.Serialize(ref receivedPosition);
+            transform.position = receivedPosition;
+        }
+    }
+
     void Update() {
         if (Input.GetKey(KeyCode.RightArrow) && movedDir != -Vector2.right) {
             dir = Vector2.right;
