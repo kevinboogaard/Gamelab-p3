6bf25d68347af3db5364b1105230cfadc5f41f05
kevinboogaard <kevin.boogaard3@gmail.com>
2015-06-15 11:00:35 +0200

    network

---
+++ Multyplayer/Assets/_scripts/Network/NetworkClass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkClass : MonoBehaviour {
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            Vector3 myPosition = transform.position;
+            stream.Serialize(ref myPosition);
+        }
+        else
+        {
+            Vector3 receivedPosition = Vector3.zero;
+            stream.Serialize(ref receivedPosition);
+            transform.position = receivedPosition;
+        }
+    }
+}
