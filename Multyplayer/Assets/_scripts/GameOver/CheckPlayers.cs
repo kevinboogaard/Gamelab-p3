b7b54c0c62b811663954c2ca91c54181dcb9db6f
Nick van Dokkum <16072@ma-web.nl>
2015-06-17 13:41:14 +0200

    created endscreen for single and multiplayer

---
+++ Multyplayer/Assets/_scripts/GameOver/CheckPlayers.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPlayers : MonoBehaviour {
+
+    public GameObject singleplayerScreen;
+    public GameObject multiplayerScreen;
+    public GameObject endScreen;
+
+    private Multyserver multyserver;
+
+    private int players;
+    private bool multiplayer = false;
+
+    void Awake() {
+        multyserver = GameObject.Find("Server").GetComponent<Multyserver>();
+    }
+    void Start() {
+        endScreen.SetActive(false);
+        InvokeRepeating("CheckSnakes", 0.25f, 0.25f);
+    }
+    void CheckSnakes() {
+        if (multyserver.playerList.Count > players) {
+            players++;
+            if (players > 1) {
+                multiplayer = true;
+            }
+        }
+        else if (multyserver.playerList.Count < players) {
+            players--;
+            if (players == 1 && multiplayer == true) {
+                endScreen.SetActive(true);
+                multiplayerScreen.SetActive(false);
+                singleplayerScreen.SetActive(true);
+            }
+            else if (players == 0) {
+                endScreen.SetActive(true);
+                multiplayerScreen.SetActive(true);
+                singleplayerScreen.SetActive(false);
+            }
+        }
+    }
+    public string snakeColor() {
+        string color = multyserver.playerList[0].name;
+        if (color == "HeadBlue") {
+            color = "blue";
+        }
+        else if (color == "HeadBrown") {
+            color = "brown";
+        }
+        else if (color == "HeadGreen") {
+            color = "green";
+        }
+        else if (color == "HeadGrey") {
+            color = "grey";
+        }
+        else if (color == "HeadWhite") {
+            color = "white";
+        }
+        else if (color == "HeadYellow") {
+            color = "yellow";
+        }
+        return (color);
+    }
+}
